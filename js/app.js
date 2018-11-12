9143eb83c1e5493e10e02da7f7312487b1f2c21f
棚原　嘉 <ysm.t02@gmail.com>
2018-11-13 00:07:01 +0900

    beach.htmlの画像マージン調整

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
-jQuery(function($){
-  $(".bg-slider").bgswitcher({
-    images: [
-    "img/background.jpg",
-    "img/background1.jpg",
-    "img/background2.jpg"
-    ], // 切り替える背景画像を指定
+jQuery(function($) {
+    $('.bg-slider').bgSwitcher({
+        images: ['img/background.jpg'], // 切り替える背景画像を指定
     });
-  });
+});
+
+
+$('.tagline').letterfx({"fx":"fall", "letter_end":"stay"});
+
+// $('.tagline').arctext({
+//     radius: 400,
+// });
